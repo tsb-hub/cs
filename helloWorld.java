62bcb86ad9b195813f011842708c41e0d9e6dfe8
Tigran Safronov Babayan <tsafronovbabayan80@stuy.edu>
2026-09-15 22:15:13 -0400

    Added the method repeatString which allows for the repetition of the initial greeting string.

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,12 @@
 public class helloWorld {
   public static void main(String[] args){
-    System.out.println("Hello World! I am Tigran.");
+    String greeting = "Hello World! I am Tigran.";
+    repeatString(greeting, 6);
+  }
+
+  public static void repeatString(String s, int i){
+    for (int x = 0; x < i; x++){
+      System.out.println(s);
+    }
   }
 }
